6f68a0b54d7554914fc9d757a4c6deeda104b1bc
Leo <danielesquivel00.lded@gmail.com>
2023-04-09 01:11:39 -0600

    UPDATE: Cambio en la forma de conexión con MongoDB

--- API/database/config.database.ts
+++ API/database/config.database.ts
@@ -1,23 +1,25 @@
 import { MongoClient, ServerApiVersion }  from 'mongodb';
+import mongoose from 'mongoose';
 
 const uri = process.env.MONGO_DB_URI;
 
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
+// const client = new MongoClient(uri, {
+//     serverApi: {
+//         version: ServerApiVersion.v1,
+//         strict: true,
+//         deprecationErrors: true,
+//     }
+// });
 
 export async function run() {
     try {
-        await client.connect();
+        await mongoose.connect(uri)
+        // await client.connect();
         // await client.db("admin").command({ping: 1});
     } catch (error) {
         return Error("Error en la conexión a la base de datos")        
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 
